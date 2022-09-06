69222f4ae877368f496d1d22c7473f3d77ff633f
Serhii Vandenvin <sevandenvin@gmail.com>
2022-09-06 16:24:56 +0300

    task 03 completed

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', onSubmitClick);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      }
+      reject({ position, delay });
+    }, delay);
+  });
+}
+
+function onSubmitClick(event) {
+  event.preventDefault();
+
+  let delay = Number(event.target.delay.value);
+  let step = Number(event.target.step.value);
+  let amount = Number(event.target.amount.value);
+
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
+    delay = delay + step;
   }
 }
